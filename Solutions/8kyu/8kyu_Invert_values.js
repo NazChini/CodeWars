f5e02bd7131d02cf10618f0a083e1910050da063
Nazneen Chinikamwala <89296394+NazChini@users.noreply.github.com>
2022-04-01 18:37:36 +0200

    Create 8kyu_Invert_values.js

---
+++ Solutions/8kyu/8kyu_Invert_values.js
@@ -0,0 +1,6 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+// Topics trained : FUNDAMENTALS | INTEGERS | NUMBERS | LISTS | DATA STRUCTURES
+
+function invert(array) {
+   return array.map(num => -num);
+}
