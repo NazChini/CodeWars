6ba78fe4552bdea76bae86c4fb9e9d765ed38a90
Nazneen Chinikamwala <89296394+NazChini@users.noreply.github.com>
2022-04-06 17:54:23 +0200

    Add 8kyu_split_string.js via upload

---
+++ Solutions/8kyu/8kyu_split_string.js
@@ -0,0 +1,6 @@
+// Write a function to split a string and convert it into an array of words.
+// Topics tested : FUNDAMENTALS | ARRAYS | STRINGS
+
+function stringToArray(string) {
+  return string.split(" ");
+}
